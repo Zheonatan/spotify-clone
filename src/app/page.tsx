0a2a60de1f6eb4f51d28c898d6c2ddd0a802c76c
ruan_alberto <ruanalbertosilva@gmail.com>
2025-03-09 16:44:44 -0300

    primeiro commit Clone Spotify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,267 @@
+import {
+  HomeIcon,
+  Search,
+  Library,
+  ChevronLeft,
+  ChevronRight,
+  Play,
+  Shuffle,
+  SkipBack,
+  SkipForward,
+  Repeat,
+  Mic2,
+  LayoutList,
+  Laptop2,
+  Volume,
+  Maximize2,
+} from "lucide-react";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+          </div>
+          <nav className="space-y-5 mt-10">
+            <a
+              href=""
+              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
+            >
+              <HomeIcon />
+              Home
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
+            >
+              <Search />
+              Search
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
+            >
+              <Library />
+              Library
+            </a>
+          </nav>
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Rock Hits
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Top Brasil
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Hits do Momento
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              OldSchool Hip Hop
+            </a>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-4">
+            <button className="p-1 rounded-full bg-black/40">
+              <ChevronLeft />
+            </button>
+            <button className="p-1 rounded-full bg-black/40">
+              <ChevronRight />
+            </button>
+          </div>
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a className="bg-white/5 group hover:bg-white/10 transition-colors rounded-md flex items-center gap-4 overflow-hidden">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={104}
+                height={104}
+              />
+              <strong>Californication</strong>
+              <button className="bg-green-400 rounded-full p-2 text-black ml-auto mr-8 invisible group-hover:visible ">
+                <Play fill='bg-black' />
+              </button>
+            </a>
+            <a className="bg-white/5 group hover:bg-white/10 transition-colors rounded-md flex items-center gap-4 overflow-hidden">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={104}
+                height={104}
+              />
+              <strong>Californication</strong>
+              <button className="bg-green-400 rounded-full p-2 text-black ml-auto mr-8 invisible group-hover:visible ">
+                <Play fill='bg-black' />
+              </button>
+            </a>
+            <a className="bg-white/5 group hover:bg-white/10 transition-colors rounded-md flex items-center gap-4 overflow-hidden">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={104}
+                height={104}
+              />
+              <strong>Californication</strong>
+              <button className="bg-green-400 rounded-full p-2 text-black ml-auto mr-8 invisible group-hover:visible ">
+                <Play fill='bg-black' />
+              </button>
+            </a>
+            <a className="bg-white/5 group hover:bg-white/10 transition-colors rounded-md flex items-center gap-4 overflow-hidden">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={104}
+                height={104}
+              />
+              <strong>Californication</strong>
+              <button className="bg-green-400 rounded-full p-2 text-black ml-auto mr-8 invisible group-hover:visible ">
+                <Play fill='bg-black' />
+              </button>
+            </a>
+            <a className="bg-white/5 group hover:bg-white/10 transition-colors rounded-md flex items-center gap-4 overflow-hidden">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={104}
+                height={104}
+              />
+              <strong>Californication</strong>
+              <button className="bg-green-400 rounded-full p-2 text-black ml-auto mr-8 invisible group-hover:visible ">
+                <Play fill='bg-black' />
+              </button>
+            </a>
+            <a className="bg-white/5 group hover:bg-white/10 transition-colors rounded-md flex items-center gap-4 overflow-hidden">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={104}
+                height={104}
+              />
+              <strong>Californication</strong>
+              <button className="bg-green-400 rounded-full p-2 text-black ml-auto mr-8 invisible group-hover:visible ">
+                <Play fill='bg-black' />
+              </button>
+            </a>
+          </div>
+          <h2 className="font-semibold text-2xl mt-10 hover:underline ">
+            Made for Ruan Silva
+          </h2>
+          <div className="grid grid-cols-8 gap-4 mt-4">
+            <a className="bg-white/5 hover:bg-white/10 p-3 rounded-md flex flex-col ">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={120}
+                height={120}
+                className="w-full"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-xs text-zinc-500">
+                Stephen Warrel, Coins, Girl in red and more
+              </span>
+            </a>
+            <a className="bg-white/5 hover:bg-white/10 p-3 rounded-md flex flex-col">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={120}
+                height={120}
+                className="w-full"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-xs text-zinc-500">
+                Stephen Warrel, Coins, Girl in red and more
+              </span>
+            </a>
+            <a className="bg-white/5 hover:bg-white/10 p-3 rounded-md flex flex-col">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={120}
+                height={120}
+                className="w-full"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-xs text-zinc-500">
+                Stephen Warrel, Coins, Girl in red and more
+              </span>
+            </a>
+            <a className="bg-white/5 hover:bg-white/10 p-3 rounded-md flex flex-col">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={120}
+                height={120}
+                className="w-full"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-xs text-zinc-500">
+                Stephen Warrel, Coins, Girl in red and more
+              </span>
+            </a>
+            <a className="bg-white/5 hover:bg-white/10 p-3 rounded-md flex flex-col">
+              <Image
+                src="/redhot-album.jpg"
+                alt="album"
+                width={120}
+                height={120}
+                className="w-full "
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-xs text-zinc-500">
+                Stephen Warrel, Coins, Girl in red and more
+              </span>
+            </a>
+          </div>
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image src="/redhot-album.jpg" alt="album" width={56} height={56} />
+          <div className="flex flex-col">
+            <strong className="font-normal">Californication</strong>
+            <span className="text-xs text-zinc-400">
+              Red Hot Chilli Peppers
+            </span>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-6">
+          <Shuffle size={20} className="text-zinc-200" />
+          <SkipBack size={20} className="text-zinc-200" />
+          <button className="h-10 w-10 bg-white rounded-full p-2 text-black ml-auto   ">
+            <Play fill='bg-black' />
+          </button>
+          <SkipForward size={20} className="text-zinc-200"/>
+          <Repeat size={20} className="text-zinc-200"/>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:47</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="h-1 rounded-full w-40 bg-zinc-200" ></div>
+            </div>
+            <span className="text-xs text-zinc-400">3:12</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <Mic2 size={20}/>
+          <LayoutList size={20}/>
+          <Laptop2 size={20}/>
+          <div className="flex items-center gap-2">
+          <Volume  size={20}/>
+          <div className="h-1 rounded-full w-16 bg-zinc-600">
+              <div className="h-1 rounded-full w-12 bg-zinc-200" ></div>
+            </div>
+          </div>
+          <Maximize2 size={20}/>
+        </div>
       </footer>
     </div>
   );
